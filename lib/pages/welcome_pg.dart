e50ddc9f6bddc7ee7c8bf1532e4290dcc76649ee
rounak <rounak610@gmail.com>
2022-01-22 22:51:27 +0530

    this is the first commit

---
+++ lib/pages/welcome_pg.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:oxon_app/pages/sigup_pg.dart';
+import 'login_pg.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/sign_in_pg.png"),
+                      fit: BoxFit.cover
+                    )
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 460, 0, 0),
+                      child: ConstrainedBox(
+                        constraints: const BoxConstraints.tightFor(width: 190, height: 60),
+                        child: ElevatedButton(
+                          onPressed: (){
+                            Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()));
+                          },
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.green[900],
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.green[50],
+                              shape: new RoundedRectangleBorder(
+                                  borderRadius: new BorderRadius.circular(35.0)
+                              )
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Don't have an account?",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.green[50],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context)=>SignupPage()));
+                          },
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.green[50],
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ] ,
+                ),
+
+
+              ],
+          ),
+        ),
+    );
+  }
+}
