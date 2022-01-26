ffd9538b46d460b9b071f4431efbbe6595666e9c
Dhruv-Varshney-developer <f20200865@pilani.bits-pilani.ac.in>
2022-01-26 16:17:15 +0530

    productspage() sorted

--- lib/pages/welcome_pg.dart
+++ lib/pages/welcome_pg.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:oxon_app/pages/donate_dustbin.dart';
-import 'package:oxon_app/pages/preview_report.dart';
-import 'package:oxon_app/pages/products_pg.dart';
 import 'package:oxon_app/pages/sigup_pg.dart';
 import 'login_pg.dart';
+import '../widgets/custom_drawer.dart';
+import 'package:oxon_app/pages/products_pg.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -19,81 +18,82 @@ class _WelcomePageState extends State<WelcomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body: Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
+        body: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
                       image: AssetImage("assets/images/sign_in_pg.png"),
-                      fit: BoxFit.cover
-                    )
+                      fit: BoxFit.cover)),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 460, 0, 0),
+                  child: ConstrainedBox(
+                    constraints:
+                        const BoxConstraints.tightFor(width: 190, height: 60),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProductsPage()));
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.green[900],
+                            fontWeight: FontWeight.bold),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.green[50],
+                          shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(35.0))),
+                    ),
                   ),
                 ),
-                Column(
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 460, 0, 0),
-                      child: ConstrainedBox(
-                        constraints: const BoxConstraints.tightFor(width: 190, height: 60),
-                        child: ElevatedButton(
-                          onPressed: (){
-                            Navigator.push(context, MaterialPageRoute(builder: (context)=>ProductsPage()));
-                          },
-                          child: Text(
-                            'Login',
-                            style: TextStyle(
-                                fontSize: 30,
-                                color: Colors.green[900],
-                                fontWeight: FontWeight.bold
-                            ),
-                          ),
-                          style: ElevatedButton.styleFrom(
-                              primary: Colors.green[50],
-                              shape: new RoundedRectangleBorder(
-                                  borderRadius: new BorderRadius.circular(35.0)
-                              )
-                          ),
-                        ),
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.green[50],
                       ),
                     ),
                     SizedBox(
-                      height: 10,
+                      width: 10,
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("Don't have an account?",
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.green[50],
-                          ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignupPage()));
+                      },
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.green[50],
+                          fontWeight: FontWeight.bold,
                         ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(context, MaterialPageRoute(builder: (context)=>PreviewReport()));
-                          },
-                          child: Text(
-                            "Sign Up",
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.green[50],
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        )
-                      ],
+                      ),
                     )
-                  ] ,
-                ),
-
-
+                  ],
+                )
               ],
-          ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
