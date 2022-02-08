f62d0a04aa688dc3db7829d58209e9ef50907726
rounak <rounak610@gmail.com>
2022-02-08 23:42:33 +0530

    twitter_integration

--- lib/pages/preview_report.dart
+++ lib/pages/preview_report.dart
@@ -235,7 +235,7 @@ class _PreviewReportState extends State<PreviewReport> {
                         constraints: const BoxConstraints.tightFor(
                             width: 250, height: 60),
                         child: ElevatedButton(
-                          onPressed: () => _onShare(context, issueType, description, imagePath),
+                          onPressed: () => _onShare(context,imagePath, issueType, description),
                           child: Text(
                             'Confirm',
                             style: TextStyle(
@@ -313,12 +313,11 @@ class _PreviewReportState extends State<PreviewReport> {
   void _onShare(BuildContext context, String imagePath, String issueType, String description) async {
     final box = context.findRenderObject() as RenderBox?;
     List<String> imagePaths = [imagePath];
-
+    String str = "@JyotilNC13 @AnitaBhandelajm @bhajanlaljatav @JaipurNigam \n$issueType \n$description \nComplaint posted by @oxon_life";
     if (imagePath.isNotEmpty) {
       await Share.shareFiles(
         imagePaths,
-          text: issueType,
-          subject: description,
+          text: str,
           sharePositionOrigin: box!.localToGlobal(Offset.zero) & box.size);
     } else {
       await Share.share(issueType,
