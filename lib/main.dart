b13c4a1c44d0e09c438047994cc5bc7715984a34
Dhruv-Varshney-developer <f20200865@pilani.bits-pilani.ac.in>
2022-02-13 04:03:00 +0530

    App not registering phone numbers issue resolved.

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'package:oxon_app/pages/take_picture.dart';
 import 'package:oxon_app/pages/welcome_pg.dart';
 import 'package:oxon_app/size_config.dart';
 import 'package:oxon_app/theme/app_theme.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 List<CameraDescription> cameras = [];
 
@@ -33,32 +34,56 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return OrientationBuilder(builder: (context, orientation) {
-      return LayoutBuilder(
-
-          builder: (context, constraints) {
-            SizeConfig().init(constraints, orientation);
-            return MaterialApp(
-              theme: AppTheme.define(),
-              debugShowCheckedModeBanner: false,
-              home: WelcomePage(),
-              routes: {
-                RaiseConcernDirect.routeName: (context) => RaiseConcernDirect(),
-                TakePictureScreen.routeName: (context) =>
-                    TakePictureScreen(camera: cameras[0]),
-                ProfilePage.routeName: (context) => ProfilePage(),
-                //Change later //title: 'John Doe'
-                SusMapping.routeName: (context) => SusMapping(),
-                //Change Later //title: 'Title'
-                DonateDustbin.routeName: (context) => DonateDustbin(),
-                ProductsPage.routeName: (context) => ProductsPage(),
-                ComingSoon.routeName: (context) => ComingSoon(),
-                PreviewReport.routeName: (context) => PreviewReport()
-              },
-            );
-          });
+      return LayoutBuilder(builder: (context, constraints) {
+        SizeConfig().init(constraints, orientation);
+        return MaterialApp(
+          theme: AppTheme.define(),
+          debugShowCheckedModeBanner: false,
+          home: InitializerWidget(),
+          routes: {
+            RaiseConcernDirect.routeName: (context) => RaiseConcernDirect(),
+            TakePictureScreen.routeName: (context) =>
+                TakePictureScreen(camera: cameras[0]),
+            ProfilePage.routeName: (context) => ProfilePage(),
+            //Change later //title: 'John Doe'
+            SusMapping.routeName: (context) => SusMapping(),
+            //Change Later //title: 'Title'
+            DonateDustbin.routeName: (context) => DonateDustbin(),
+            ProductsPage.routeName: (context) => ProductsPage(),
+            ComingSoon.routeName: (context) => ComingSoon(),
+            PreviewReport.routeName: (context) => PreviewReport()
+          },
+        );
+      });
     });
+  }
+}
 
+class InitializerWidget extends StatefulWidget {
+  const InitializerWidget({Key? key}) : super(key: key);
+
+  @override
+  _InitializerWidgetState createState() => _InitializerWidgetState();
+}
+
+class _InitializerWidgetState extends State<InitializerWidget> {
+  late FirebaseAuth _auth;
+  late User _user;
+  bool isLoading = true;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _auth = FirebaseAuth.instance;
+    _user = _auth.currentUser!;
+    isLoading = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoading? Scaffold(
+      body: Center(child: CircularProgressIndicator()),
+    ):_user==null?WelcomePage():SusMapping();
   }
 }
