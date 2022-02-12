2dc40ce54ac0dc897fe09243290d296240438073
Dhruv-Varshney-developer <f20200865@pilani.bits-pilani.ac.in>
2022-02-12 19:28:47 +0530

    phone auth in progress

--- lib/pages/otpscreen.dart
+++ lib/pages/otpscreen.dart
@@ -83,42 +83,25 @@ class _OTPScreenState extends State<OTPScreen> {
                       Padding(
                         padding: const EdgeInsets.all(30.0),
                         child: PinPut(
-                          fieldsCount: 6,
-                          textStyle: const TextStyle(
-                              fontSize: 25.0, color: Colors.white),
-                          eachFieldWidth: 40.0,
-                          eachFieldHeight: 55.0,
-                          focusNode: _pinPutFocusNode,
-                          controller: _pinPutController,
-                          submittedFieldDecoration: pinPutDecoration,
-                          selectedFieldDecoration: pinPutDecoration,
-                          followingFieldDecoration: pinPutDecoration,
-                          pinAnimationType: PinAnimationType.fade,
-                          onSubmit: (pin) async {
-                            try {
-                              await FirebaseAuth.instance
-                                  .signInWithCredential(
-                                      PhoneAuthProvider.credential(
-                                          verificationId: _verificationCode,
-                                          smsCode: pin))
-                                  .then((value) async {
-                                if (value.user != null) {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => SusMapping()),
-                                  );
-                                }
-                              });
-                            } catch (e) {
-                              FocusScope.of(context).unfocus();
-                              _scaffoldkey.currentState
-                                  // ignore: deprecated_member_use
-                                  ?.showSnackBar(
-                                      SnackBar(content: Text('Invalid OTP')));
-                            }
-                          },
-                        ),
+                            fieldsCount: 6,
+                            textStyle: const TextStyle(
+                                fontSize: 25.0, color: Colors.white),
+                            eachFieldWidth: 40.0,
+                            eachFieldHeight: 55.0,
+                            focusNode: _pinPutFocusNode,
+                            controller: _pinPutController,
+                            submittedFieldDecoration: pinPutDecoration,
+                            selectedFieldDecoration: pinPutDecoration,
+                            followingFieldDecoration: pinPutDecoration,
+                            pinAnimationType: PinAnimationType.fade,
+                            onSubmit: (pin) async {
+                              PhoneAuthCredential phoneAuthCredential =
+                                  PhoneAuthProvider.credential(
+                                      verificationId: _verificationCode,
+                                      smsCode: pin);
+                              signinWithPhoneAuthCredential(
+                                  phoneAuthCredential);
+                            }),
                       )
                     ],
                   ),
