5f300f4c7431e6248cb771a5e9773a9a11a6842f
Vedant Panchal <vedantpanchal12345@gmail.com>
2022-02-18 14:15:18 +0530

    before refactoring

---
+++ lib/models/dustbin_data.dart
@@ -0,0 +1,73 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:oxon_app/models/loc_data.dart';
+
+class DustbinData extends LocData {
+  int overflowingCount;
+
+  DustbinData(
+      {required locatedBy,
+      required helpfulCount,
+      required uId,
+      required usersVoted,
+      required subType,
+      required type,
+      required isDisplayed,
+      required location,
+      required notPresentCount,
+      referenceId,
+      required this.overflowingCount})
+      : super(
+            locatedBy: locatedBy,
+            helpfulCount: helpfulCount,
+            uId: uId,
+            usersVoted: usersVoted,
+            subType: subType,
+            type: type,
+            isDisplayed: isDisplayed,
+            location: location,
+            notPresentCount: notPresentCount,
+            referenceId: referenceId);
+
+  factory DustbinData.fromSnapshot(DocumentSnapshot snapshot) {
+    final newDustbinData =
+        DustbinData.fromJson(snapshot.data() as Map<String, dynamic>);
+    newDustbinData.referenceId = snapshot.reference.id;
+    return newDustbinData;
+  }
+
+  factory DustbinData.fromJson(Map<String, dynamic> json) =>
+      _dustbinDataFromJson(json);
+
+  Map<String, dynamic> toJson() => _dustbinDataToJson(this);
+}
+
+DustbinData _dustbinDataFromJson(Map<String, dynamic> json) {
+  return DustbinData(
+    locatedBy: json['located_by'],
+    helpfulCount: json['helpful_count'],
+    uId: json['u_id'],
+    usersVoted: json['users_voted'],
+    subType: json['sub_type'],
+    type: json['type'],
+    isDisplayed: json['is_displayed'],
+    location: json['location'],
+    notPresentCount: json['not_present_count'],
+    overflowingCount: json['overflowing_count'],
+    referenceId: json['reference_id'],
+  );
+}
+
+Map<String, dynamic> _dustbinDataToJson(DustbinData instance) =>
+    <String, dynamic>{
+      'located_by': instance.locatedBy,
+      'helpful_count': instance.helpfulCount,
+      'u_id': instance.uId,
+      'users_voted': instance.usersVoted,
+      'sub_type': instance.subType,
+      'type': instance.type,
+      'is_displayed': instance.isDisplayed,
+      'location': instance.location,
+      'not_present_count': instance.notPresentCount,
+      'overflowing_count': instance.overflowingCount,
+      'reference_id': instance.referenceId,
+    };
---
+++ lib/models/toilet_data.dart
@@ -0,0 +1,73 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:oxon_app/models/loc_data.dart';
+
+class ToiletData extends LocData {
+  int notCleanedCount;
+
+  ToiletData(
+      {required locatedBy,
+      required helpfulCount,
+      required uId,
+      required usersVoted,
+      required subType,
+      required type,
+      required isDisplayed,
+      required location,
+      required notPresentCount,
+      referenceId,
+      required this.notCleanedCount})
+      : super(
+            locatedBy: locatedBy,
+            helpfulCount: helpfulCount,
+            uId: uId,
+            usersVoted: usersVoted,
+            subType: subType,
+            type: type,
+            isDisplayed: isDisplayed,
+            location: location,
+            notPresentCount: notPresentCount,
+            referenceId: referenceId);
+
+  factory ToiletData.fromSnapshot(DocumentSnapshot snapshot) {
+    final newToiletData =
+        ToiletData.fromJson(snapshot.data() as Map<String, dynamic>);
+    newToiletData.referenceId = snapshot.reference.id;
+    return newToiletData;
+  }
+
+  factory ToiletData.fromJson(Map<String, dynamic> json) =>
+      _toiletDataFromJson(json);
+
+  Map<String, dynamic> toJson() => _toiletDataToJson(this);
+}
+
+ToiletData _toiletDataFromJson(Map<String, dynamic> json) {
+  return ToiletData(
+    locatedBy: json['located_by'],
+    helpfulCount: json['helpful_count'],
+    uId: json['u_id'],
+    usersVoted: json['users_voted'],
+    subType: json['sub_type'],
+    type: json['type'],
+    isDisplayed: json['is_displayed'],
+    location: json['location'],
+    notPresentCount: json['not_present_count'],
+    notCleanedCount: json['not_cleaned_count'],
+    referenceId: json['reference_id'],
+  );
+}
+
+Map<String, dynamic> _toiletDataToJson(ToiletData instance) =>
+    <String, dynamic>{
+      'located_by': instance.locatedBy,
+      'helpful_count': instance.helpfulCount,
+      'u_id': instance.uId,
+      'users_voted': instance.usersVoted,
+      'sub_type': instance.subType,
+      'type': instance.type,
+      'is_displayed': instance.isDisplayed,
+      'location': instance.location,
+      'not_present_count': instance.notPresentCount,
+      'not_cleaned_count': instance.notCleanedCount,
+      'reference_id': instance.referenceId,
+    };
