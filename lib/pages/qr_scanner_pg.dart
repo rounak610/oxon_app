15c4d9d22050db251dfb2ecea9a7a7d0f96fe6d5
Prathamesh Anwekar <f20201039@pilani.bits-pilani.ac.in>
2022-02-20 19:33:23 +0530

    done with qr_scanner

--- lib/pages/qr_scanner_pg.dart
+++ lib/pages/qr_scanner_pg.dart
@@ -21,10 +21,10 @@ class _QRScannerPageState extends State<QRScannerPage> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   Barcode? result;
   QRViewController? controller;
-
+  int? scansToday;
+  var lastScanDay;
   final FirebaseAuth auth = FirebaseAuth.instance;
   int currentUserCredits = 0;
-
 
   _fetch() async {
     final user = await FirebaseAuth.instance.currentUser;
@@ -34,7 +34,12 @@ class _QRScannerPageState extends State<QRScannerPage> {
           .doc(user.uid)
           .get()
           .then((ds) {
-        currentUserCredits = ds.data()!['credits'] == null ? 0 : ds.data()!['credits'];
+        scansToday =
+        ds.data()!['scansToday'] == null ? 0 : ds.data()!['scansToday'];
+        lastScanDay =
+        ds.data()!['lastScanDay'] == null ? 0 : ds.data()!['lastScanDay'];
+        currentUserCredits =
+        ds.data()!['credits'] == null ? 0 : ds.data()!['credits'];
       }).catchError((e) {
         print(e);
       });
@@ -42,60 +47,148 @@ class _QRScannerPageState extends State<QRScannerPage> {
   }
 
   Widget _QRResult(String? result) {
-    if (result != null) {
-      return Expanded(
-        child: Dialog(
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12.0)), //this right here
-          child: Container(
-            height: SizeConfig.screenHeight * 0.2,
-            width: SizeConfig.widthMultiplier * 80,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Text(
-                    '5 Credits added to your account!',
-                    style: TextStyle(color: Colors.green[900]),
-                  ),
+    if (result == 'oxon-van-qr' ) {
+      if (scansToday! < 2) {
+        if (lastScanDay == DateTime.now().day) {
+          return Expanded(
+            child: Dialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0)), //this right here
+              child: Container(
+                height: SizeConfig.screenHeight * 0.2,
+                width: SizeConfig.widthMultiplier * 80,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        '5 Credits added to your account!',
+                        style: TextStyle(color: Colors.green[900]),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 25.0)),
+                    TextButton(
+                        onPressed: () async {
+                          final user = await FirebaseAuth.instance.currentUser;
+                          FirebaseFirestore.instance
+                              .collection('users')
+                              .doc(user?.uid)
+                              .update({
+                            'credits': currentUserCredits + 5,
+                            'scansToday': scansToday! + 1,
+                            'lastScanDay': DateTime.now().day
+                          }).then((value) {
+                            Navigator.of(context)
+                                .pushNamed(ProfilePage.routeName);
+                          }).catchError((e) {
+                            print(e);
+                          });
+                          await Navigator.of(context)
+                              .pushReplacementNamed(ProfilePage.routeName);
+                        },
+                        child: Text(
+                          'Got It!',
+                          style:
+                          TextStyle(color: Colors.purple, fontSize: 18.0),
+                        ))
+                  ],
                 ),
-                Padding(padding: EdgeInsets.only(top: 25.0)),
-                TextButton(
-                    onPressed: () async {
-                      final user = await FirebaseAuth.instance.currentUser;
-                      FirebaseFirestore.instance
-                          .collection('users')
-                          .doc(user?.uid)
-                          .update({
-                        'credits': currentUserCredits + 5
-                      }).then((value) {
-                        Navigator.of(context)
-                            .pushNamed(ProfilePage.routeName);
-                      }).catchError((e) {
-                        print(e);
-                      });
-                      await Navigator.of(context)
-                          .pushReplacementNamed(ProfilePage.routeName);
-                    },
+              ),
+            ),
+          );
+        } else {
+          return Expanded(
+            child: Dialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0)), //this right here
+              child: Container(
+                height: SizeConfig.screenHeight * 0.2,
+                width: SizeConfig.widthMultiplier * 80,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        '5 Credits added to your account!',
+                        style: TextStyle(color: Colors.green[900]),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 25.0)),
+                    TextButton(
+                        onPressed: () async {
+                          final user = await FirebaseAuth.instance.currentUser;
+                          FirebaseFirestore.instance
+                              .collection('users')
+                              .doc(user?.uid)
+                              .update({
+                            'credits': currentUserCredits + 5,
+                            'scansToday': 1,
+                            'lastScanDay': DateTime.now().day
+                          }).then((value) {
+                            Navigator.of(context)
+                                .pushNamed(ProfilePage.routeName);
+                          }).catchError((e) {
+                            print(e);
+                          });
+                          await Navigator.of(context)
+                              .pushReplacementNamed(ProfilePage.routeName);
+                        },
+                        child: Text(
+                          'Got It!',
+                          style:
+                          TextStyle(color: Colors.purple, fontSize: 18.0),
+                        ))
+                  ],
+                ),
+              ),
+            ),
+          );
+        }
+      } else {
+        return Expanded(
+          child: Dialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0)), //this right here
+            child: Container(
+              height: SizeConfig.screenHeight * 0.2,
+              width: SizeConfig.widthMultiplier * 80,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
                     child: Text(
-                      'Got It!',
-                      style: TextStyle(color: Colors.purple, fontSize: 18.0),
-                    ))
-              ],
+                      'Sorry, you have surpassed today\'s scan limit.',
+                      style: TextStyle(color: Colors.green[900]),
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 25.0)),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context)
+                            .pushReplacementNamed(ProfilePage.routeName);
+                      },
+                      child: Text(
+                        'Got It!',
+                        style: TextStyle(color: Colors.purple, fontSize: 18.0),
+                      ))
+                ],
+              ),
             ),
           ),
-        ),
-      );
+        );
+      }
     }
 
     return Expanded(
         flex: 1,
         child: Center(
             child: Text(
-          'Please scan a valid QR code.',
-          style: TextStyle(fontSize: 20),
-        )));
+              'Please scan a valid QR code.',
+              style: TextStyle(fontSize: 20),
+            )));
   }
 
   // In order to get hot reload to work we need to pause the camera if the platform
