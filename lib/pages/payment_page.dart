c456f8222bfdb32e4787b67d22e0b280474d446f
Prathamesh Anwekar <f20201039@pilani.bits-pilani.ac.in>
2022-02-21 22:14:43 +0530

    added credit system logic

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -9,21 +9,75 @@ import 'cart_pg.dart';
 class PaymentPage extends StatefulWidget {
   final int price;
 
-  PaymentPage( {required this.price});
+  PaymentPage({required this.price});
   @override
   _PaymentPageState createState() => _PaymentPageState();
 }
 
 class _PaymentPageState extends State<PaymentPage> {
+  int? userCredits;
+  int? finalPrice;
+
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  _fetch() async {
+    int price = widget.price;
+
+    final user = await FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .get()
+          .then((ds) {
+        print(ds);
+        userCredits = ds.data()!['credits'] == null ? 0 : ds.data()!['credits'];
+
+        if (price! * 0.2 >= userCredits!) {
+         FirebaseFirestore.instance
+              .collection('users')
+              .doc(user?.uid)
+              .update({
+                'credits': 0,
+              })
+              .then((value) {})
+              .catchError((e) {
+                print(e);
+              });
+          finalPrice = (price! * 0.8).toInt();
+        } else {
+          FirebaseFirestore.instance
+              .collection('users')
+              .doc(user?.uid)
+              .update({
+                'credits': userCredits! - (0.2 * price!).toInt(),
+              })
+              .then((value) {})
+              .catchError((e) {
+                print(e);
+              });
+          finalPrice = (price! * 0.8).toInt();
+        }
+      }).catchError((e) {
+        print(e);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Center(child: Text("PAY your total = "+widget.price.toString())),
-        ],
-      ),
-    );
+    return FutureBuilder(
+        future: _fetch(),
+        builder: (context, snapshot) {
+          return Scaffold(
+            body: Stack(
+              children: [
+                Center(
+                    child: Text("PAY your total = " +
+                        finalPrice.toString())),
+              ],
+            ),
+          );
+        });
   }
 }
