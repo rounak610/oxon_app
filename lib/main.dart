606f34b2d7ea81b76b26be447a29f19c443c9bad
Vedant Panchal <vedantpanchal12345@gmail.com>
2022-02-23 11:41:42 +0530

    checkpoint

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:oxon/pages/cart_pg.dart';
 import 'package:oxon/pages/coming_soon.dart';
 import 'package:oxon/pages/donate_dustbin.dart';
 import 'package:oxon/pages/driver_passcode.dart';
