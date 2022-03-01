4ea747090fa1cdf0d14edd3373d8208ea90fe1cb
Prathamesh Anwekar <f20201039@pilani.bits-pilani.ac.in>
2022-03-01 21:23:39 +0530

    changes in cart page

--- lib/pages/cart_pg.dart
+++ lib/pages/cart_pg.dart
@@ -72,172 +72,177 @@ class _CartPageState extends State<CartPage> {
         drawer: CustomDrawer(),
         appBar: CustomAppBar(context, 'Cart'),
         backgroundColor: Color.fromARGB(255, 34, 90, 0),
-        body: Stack(
-          children: [
-            Padding(
-                padding: EdgeInsets.fromLTRB(20.0, 5, 15.0, 5),
-                child: Text(
-                  "Swipe left to delete a product from the cart",
-                  style: TextStyle(
-                      color: AppColors().oxonOffWhite,
-                      fontWeight: FontWeight.bold),
-                )),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              child: Container(
-                margin: EdgeInsets.all(15.0),
-                padding: EdgeInsets.all(15.0),
-                child: Stack(
-                  children: [
-                    FutureBuilder<QuerySnapshot>(
-                      future: _userRef.doc(_user?.uid).collection("Cart").get(),
-                      builder: (context, snapshot) {
-                        if (snapshot.hasError) {
-                          return Scaffold(
-                            body: Center(
-                              child: Text("Error Loading products"),
-                            ),
-                          );
-                        }
-                        if (snapshot.connectionState == ConnectionState.done) {
-                          return Scaffold(
-                            backgroundColor: AppColors().oxonGreen,
-                            body: Column(
-                              children: [
-                                SizedBox(
-                                  height: SizeConfig.screenHeight * 0.5,
-                                  child: ListView(
-                                    padding: EdgeInsets.only(
-                                      top: 30.0,
-                                      bottom: 20.0,
-                                    ),
-                                    children:
-                                        snapshot.data!.docs.map((document) {
-                                      int p = document.get('price') as int;
-                                      int q = document.get('quantity') as int;
-                                      int d = document.get('delivery') as int;
-                                      total += p * q;
-
-                                      return CartItem(
-                                          "${document.get('ID')}",
-                                          document.get('price'),
-                                          document.get('quantity'),
-                                          "${document.get('name')}");
-                                    }).toList(),
-                                  ),
+        body: SingleChildScrollView(
+          child: SizedBox(
+            height: SizeConfig.screenHeight ,
+            child: Stack(
+              children: [
+                Padding(
+                    padding: EdgeInsets.fromLTRB(20.0, 5, 15.0, 5),
+                    child: Text(
+                      "Swipe left to delete a product from the cart",
+                      style: TextStyle(
+                          color: AppColors().oxonOffWhite,
+                          fontWeight: FontWeight.bold),
+                    )),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    padding: EdgeInsets.all(15.0),
+                    child: Stack(
+                      children: [
+                        FutureBuilder<QuerySnapshot>(
+                          future: _userRef.doc(_user?.uid).collection("Cart").get(),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasError) {
+                              return Scaffold(
+                                body: Center(
+                                  child: Text("Error Loading products"),
                                 ),
-                                Positioned(
-                                  bottom: SizeConfig.screenHeight * 0.2,
-                                  left: 35,
-                                  child: FutureBuilder(
-                                      future: _fetch(),
-                                      builder: (context, snapshot) {
-                                        if (snapshot.hasError) {
-                                          return Center(
-                                            child: Text("Loading"),
-                                          );
-                                        }
-                                        return Column(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.start,
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              Container(
-                                                  child: Text(
-                                                'Your cart total is: Rs. $total ',
-                                                textAlign: TextAlign.start,
-                                                style: TextStyle(
-                                                    color: Colors.white),
-                                              )),
-                                              Container(
-                                                  child: Text(
-                                                'Discount applied from wallet (MAX 20%): Rs. $discount ',
-                                                style: TextStyle(
-                                                    color: Colors.white),
-                                              )),
-                                              Container(
-                                                  child: Text(
-                                                'Delivery Charges: Rs. 49 ',
-                                                style: TextStyle(
-                                                    color: Colors.white),
-                                              )),
-                                              Container(
-                                                  child: Text(
-                                                'Total amount to be paid: Rs. ${total - discount + 49} ',
-                                                style: TextStyle(
-                                                    color: Colors.white),
-                                              )),
-                                            ]);
-                                      }),
-                                ),
-                                Positioned(
-                                  bottom: SizeConfig.screenHeight * 0.1,
-                                  right: 15,
-                                  left: 15,
-                                  child: Center(
-                                    child: GestureDetector(
-                                      onTap: () async {
-                                        int price = total;
-                                        final user = await FirebaseAuth
-                                            .instance.currentUser;
-                                        await Navigator.of(context).push(
-                                            MaterialPageRoute(
-                                                builder: (context) => (Payment(
-                                                    total - discount + 49))));
-                                      },
-                                      child: Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                          vertical: 15.0,
-                                          horizontal: 10.0,
+                              );
+                            }
+                            if (snapshot.connectionState == ConnectionState.done) {
+                              return Scaffold(
+                                backgroundColor: AppColors().oxonGreen,
+                                body: Column(
+                                  children: [
+                                    SizedBox(
+                                      height: SizeConfig.screenHeight * 0.7,
+                                      child: ListView(
+                                        padding: EdgeInsets.only(
+                                          top: 30.0,
+                                          bottom: 20.0,
                                         ),
-                                        child: Center(
-                                          child: Container(
-                                            decoration: BoxDecoration(
-                                                borderRadius:
-                                                    BorderRadius.circular(20.0),
-                                                color:
-                                                    AppColors().oxonOffWhite),
-                                            child: Padding(
-                                              padding:
-                                                  const EdgeInsets.symmetric(
-                                                vertical: 15.0,
-                                                horizontal: 25.0,
+                                        children:
+                                            snapshot.data!.docs.map((document) {
+                                          int p = document.get('price') as int;
+                                          int q = document.get('quantity') as int;
+                                          int d = document.get('delivery') as int;
+                                          total += p * q;
+                  
+                                          return CartItem(
+                                              "${document.get('ID')}",
+                                              document.get('price'),
+                                              document.get('quantity'),
+                                              "${document.get('name')}");
+                                        }).toList(),
+                                      ),
+                                    ),
+                                    Positioned(
+                                      bottom: SizeConfig.screenHeight * 0.2,
+                                      left: 35,
+                                      child: FutureBuilder(
+                                          future: _fetch(),
+                                          builder: (context, snapshot) {
+                                            if (snapshot.hasError) {
+                                              return Center(
+                                                child: Text("Loading"),
+                                              );
+                                            }
+                                            return Column(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.start,
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  Container(
+                                                      child: Text(
+                                                    'Your cart total is: Rs. $total ',
+                                                    textAlign: TextAlign.start,
+                                                    style: TextStyle(
+                                                        color: Colors.white),
+                                                  )),
+                                                  Container(
+                                                      child: Text(
+                                                    'Discount applied from wallet (MAX 20%): Rs. $discount ',
+                                                    style: TextStyle(
+                                                        color: Colors.white),
+                                                  )),
+                                                  Container(
+                                                      child: Text(
+                                                    'Delivery Charges: Rs. 49 ',
+                                                    style: TextStyle(
+                                                        color: Colors.white),
+                                                  )),
+                                                  Container(
+                                                      child: Text(
+                                                    'Total amount to be paid: Rs. ${total - discount + 49} ',
+                                                    style: TextStyle(
+                                                        color: Colors.white),
+                                                  )),
+                                                ]);
+                                          }),
+                                    ),
+                                    Positioned(
+                                      bottom: SizeConfig.screenHeight * 0.1,
+                                      right: 15,
+                                      left: 15,
+                                      child: Center(
+                                        child: GestureDetector(
+                                          onTap: () async {
+                                            int price = total;
+                                            final user = await FirebaseAuth
+                                                .instance.currentUser;
+                                            await Navigator.of(context).push(
+                                                MaterialPageRoute(
+                                                    builder: (context) => (Payment(
+                                                        total - discount + 49))));
+                                          },
+                                          child: Padding(
+                                            padding: const EdgeInsets.symmetric(
+                                              vertical: 15.0,
+                                              horizontal: 10.0,
+                                            ),
+                                            child: Center(
+                                              child: Container(
+                                                decoration: BoxDecoration(
+                                                    borderRadius:
+                                                        BorderRadius.circular(20.0),
+                                                    color:
+                                                        AppColors().oxonOffWhite),
+                                                child: Padding(
+                                                  padding:
+                                                      const EdgeInsets.symmetric(
+                                                    vertical: 15.0,
+                                                    horizontal: 25.0,
+                                                  ),
+                                                  child: Text("Proceed to payment",
+                                                      style: TextStyle(
+                                                          fontSize: 20,
+                                                          color: AppColors()
+                                                              .oxonGreen)),
+                                                ),
                                               ),
-                                              child: Text("Proceed to payment",
-                                                  style: TextStyle(
-                                                      fontSize: 20,
-                                                      color: AppColors()
-                                                          .oxonGreen)),
                                             ),
                                           ),
                                         ),
                                       ),
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                          );
-                        }
-                        return Scaffold(
-                          backgroundColor: Colors.transparent,
-                          body: Center(
-                            child: CircularProgressIndicator(
+                                    )
+                                  ],
+                                ),
+                              );
+                            }
+                            return Scaffold(
                               backgroundColor: Colors.transparent,
-                              color: Colors.white,
-                            ),
-                          ),
-                        );
-                      },
+                              body: Center(
+                                child: CircularProgressIndicator(
+                                  backgroundColor: Colors.transparent,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ));
     ;
   }
