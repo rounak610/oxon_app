89611523b9dd4681a7b4480be7810b3e4fe965d2
Vedant Panchal <vedantpanchal12345@gmail.com>
2022-03-03 20:24:55 +0530

    only one of the void signInWith... function kept

--- lib/pages/otpscreen.dart
+++ lib/pages/otpscreen.dart
@@ -1,13 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:oxon/pages/products_pg.dart';
+import 'package:oxon/pages/sustainable_mapping_pg.dart';
 import 'package:oxon/pages/update_profile.dart';
 import 'package:oxon/size_config.dart';
 import 'package:oxon/theme/app_theme.dart';
 import 'package:oxon/widgets/Appbar_otpscreen.dart';
 import 'package:pinput/pin_put/pin_put.dart';
 import 'package:timer_button/timer_button.dart';
+
+import '../user_data.dart';
 
 class OTPScreen extends StatefulWidget {
   final String phone;
@@ -36,62 +38,6 @@ class _OTPScreenState extends State<OTPScreen> {
 
   @override
   Widget build(BuildContext context) {
-    void signinWithPhoneAuthCredential(
-        PhoneAuthCredential phoneAuthCredential) async {
-      final QuerySnapshot result = await firestore
-          .collection('users')
-          .where('mobile', isEqualTo: widget.phone)
-          .limit(1)
-          .get();
-      setState(() {
-        showLoading = true;
-      });
-      try {
-        final authCredential = await FirebaseAuth.instance
-            .signInWithCredential(phoneAuthCredential);
-        setState(() {
-          showLoading = false;
-        });
-
-        if (result.docs.length != 0) {
-          if (authCredential.user != null) {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        ProductsPage())); ////////LOGIC FOR OLD USERS
-          }
-        } else {
-          final user = await FirebaseAuth
-              .instance.currentUser; //////////LOGIC FOR NEW USERS
-          await FirebaseFirestore.instance
-              .collection('users')
-              .doc(user?.uid)
-              .set({
-                'credits': 50,
-              })
-              .then((value) async {})
-              .catchError((e) {
-                print(e);
-              });
-          const bonusSnackBar = SnackBar(
-            content: Text('Yay!Rs. 50 signing up bonus added to wallet!!'),
-          );
-          ScaffoldMessenger.of(context).showSnackBar(bonusSnackBar);
-          await Navigator.push(context,
-              MaterialPageRoute(builder: (context) => UpdateProfile()));
-        }
-      } on FirebaseAuthException catch (e) {
-        setState(() {
-          showLoading = false;
-        });
-        FocusScope.of(context).unfocus();
-        _scaffoldkey.currentState
-            // ignore: deprecated_member_use
-            ?.showSnackBar(SnackBar(content: Text("Invaild OTP")));
-      }
-    }
-
     return Scaffold(
         key: _scaffoldkey,
         appBar: AppBarotpscreen(context, "OTP Verification"),
@@ -197,49 +143,141 @@ class _OTPScreenState extends State<OTPScreen> {
         forceResendingToken: _resendToken);
   }
 
-  void signinWithPhoneAuthCredential(
-      PhoneAuthCredential phoneAuthCredential) async {
-    final QuerySnapshot result = await firestore
-        .collection('users')
-        .where('mobile', isEqualTo: widget.phone)
-        .limit(1)
-        .get();
-    setState(() {
-      showLoading = true;
-    });
-    try {
-      final authCredential =
-          await FirebaseAuth.instance.signInWithCredential(phoneAuthCredential);
-      setState(() {
-        showLoading = false;
-      });
-
-      if (result != null) {
-        if (authCredential.user != null) {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => ProductsPage()));
-        }
-      } else {
-        if (authCredential.user != null) {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => UpdateProfile()));
-        }
-      }
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        showLoading = false;
-      });
-      FocusScope.of(context).unfocus();
-      _scaffoldkey.currentState
-          // ignore: deprecated_member_use
-          ?.showSnackBar(SnackBar(content: Text("Invaild OTP")));
-    }
-  }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _verifyPhone();
+  }
+
+  void rewardUser() async { //gives curr user as null (maybe not)
+    final user =
+        await FirebaseAuth.instance.currentUser; //////////LOGIC FOR NEW USERS
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user?.uid)
+        .set({
+          'credits': 50,
+        })
+        .then((value) async {})
+        .catchError((e) {
+          print(e);
+        });
+    const bonusSnackBar = SnackBar(
+      content: Text('Yay!Rs. 50 signing up bonus added to wallet!!'),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(bonusSnackBar);
+    await Navigator.push(
+        context, MaterialPageRoute(builder: (context) => UpdateProfile()));
+  }
+
+
+  // void signinWithPhoneAuthCredential( // only this one is used
+  //     PhoneAuthCredential phoneAuthCredential) async {
+  //   final QuerySnapshot result = await firestore
+  //       .collection('users')
+  //       .where('mobile', isEqualTo: widget.phone)
+  //       .limit(1)
+  //       .get();
+  //   setState(() {
+  //     showLoading = true;
+  //   });
+  //   try {
+  //     final authCredential = await FirebaseAuth.instance
+  //         .signInWithCredential(phoneAuthCredential);
+  //     setState(() {
+  //       showLoading = false;
+  //     });
+  //
+  //     if (result.docs.length != 0) { // it works from here 11
+  //       if (authCredential.user != null) {
+  //         // check this code
+  //         Navigator.push(
+  //             context,
+  //             MaterialPageRoute(
+  //                 builder: (context) =>
+  //                     SusMapping())); ////////LOGIC FOR OLD USERS
+  //       }
+  //     } else {
+  //       final user = await FirebaseAuth
+  //           .instance.currentUser; //////////LOGIC FOR NEW USERS
+  //       await FirebaseFirestore.instance
+  //           .collection('users')
+  //           .doc(user?.uid)
+  //           .set({
+  //         'credits': 50,
+  //       })
+  //           .then((value) async {})
+  //           .catchError((e) {
+  //         print(e);
+  //       });
+  //       const bonusSnackBar = SnackBar(
+  //         content: Text('Yay! Rs. 50 signing up bonus added to wallet!!'),
+  //       );
+  //       ScaffoldMessenger.of(context).showSnackBar(bonusSnackBar);
+  //       await Navigator.push(context,
+  //           MaterialPageRoute(builder: (context) => UpdateProfile()));
+  //     }
+  //   } on FirebaseAuthException catch (e) {
+  //     setState(() {
+  //       showLoading = false;
+  //     });
+  //     FocusScope.of(context).unfocus();
+  //     _scaffoldkey.currentState
+  //     // ignore: deprecated_member_use
+  //         ?.showSnackBar(SnackBar(content: Text("Invaild OTP")));
+  //   }
+  // }
+
+
+
+  void signinWithPhoneAuthCredential(
+      PhoneAuthCredential phoneAuthCredential) async {
+    // final QuerySnapshot result = await firestore
+    //     .collection('users')
+    //     .where('mobile', isEqualTo: widget.phone)
+    //     .limit(1)
+    //     .get();
+    setState(() {
+      showLoading = true;
+    });
+    try {
+      final authCredential =
+      await FirebaseAuth.instance.signInWithCredential(phoneAuthCredential);
+      setState(() {
+        showLoading = false;
+      });
+
+      if (UserData.mobileNumber != null) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => SusMapping()));
+      } else {
+        rewardUser();
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => UpdateProfile()));
+      }
+
+      // if (result != null) {
+      //   if (authCredential.user != null) {
+      //     Navigator.push(
+      //         context, MaterialPageRoute(builder: (context) => SusMapping()));
+      //   }
+      // } else {
+      //   if (authCredential.user != null) {
+      //     rewardUser();
+      //     Navigator.push(context,
+      //         MaterialPageRoute(builder: (context) => UpdateProfile()));
+      //   }
+      // }
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        showLoading = false;
+      });
+      FocusScope.of(context).unfocus();
+      _scaffoldkey.currentState
+      // ignore: deprecated_member_use
+          ?.showSnackBar(SnackBar(content: Text("Invaild OTP")));
+    }
   }
 }
